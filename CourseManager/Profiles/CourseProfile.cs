c616d27d95ac15b7bb6a63e53f4b1b5fb00fc3d1
Lukas <vanderspiegel.lukas@gmail.com>
2023-12-13 22:48:20 +0100

    profiling

---
+++ CourseManager/Profiles/CourseProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CourseManager.Entities;
+using CourseManager.Models;
+using CourseManager;
+
+namespace CourseManager.Profiles
+{
+    public class CourseProfile : Profile
+    {
+        public CourseProfile()
+        {
+            CreateMap<Course, CourseDto>();
+        }
+    }
+}
---
+++ CourseManager/Profiles/TeacherProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CourseManager.Entities;
+using CourseManager.Models;
+using CourseManager;
+
+namespace CourseManager.Profiles
+{
+    public class TeacherProfile : Profile
+    {
+        public TeacherProfile()
+        {
+            CreateMap<Teacher, TeacherDto>();
+        }
+    }
+}
